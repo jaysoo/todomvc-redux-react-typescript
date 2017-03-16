193bc349ac4812e23a69fb189dd9173b043ae0f0
Jack Hsu <jack@pressly.com>
2017-03-15 21:22:36 -0400

    fixes server start message for dev

--- server/index.js
+++ server/index.js
@@ -3,12 +3,13 @@ const fs = require('fs');
 const chalk = require('chalk');
 const cp = require('child_process');
 const buildDir = path.resolve(process.cwd(), 'build');
+const isProd = process.env.NODE_ENV === 'production'
 
 if (!fs.existsSync(buildDir)) {
   console.log(chalk.yellow('Build folder not found. Running webpack for you...'));
   cp.execSync(`npm run build`);
 } else {
-  console.log(chalk.yellow('Starting from previous build. If you want to create a new build, run `npm run build` first.'));
+  isProd && console.log(chalk.yellow('Starting from previous build. If you want to create a new build, run `npm run build` first.'));
 }
 
 require("./setup")({
